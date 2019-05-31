7501863da8b9022e75651d96e8cf4c7619c28d4d
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-05-31 15:24:37 +0300

    json

--- src/components/pages/Quiz.vue
+++ src/components/pages/Quiz.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="wrapper">
-        Quiz
+        {{this.catalog}}
     </div>
 </template>
 
 <script>
+import json from '../../assets/json/catalog.json';
+
 export default {
     data() {
         return {
-            //
+            catalog: null
         };
     },
+    created() {
+        this.catalog = json;
+    }
 }
 </script>
 
 <style scoped lang="scss">
 @import '../../styles/_index.scss';
 
+.wrapper {
+    padding: $m-basic;
+    background: silver;
+}
 </style>
