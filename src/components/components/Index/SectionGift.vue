304b518c8e17187d82bf5941b9595b26cfb8652b
anton-sergeenkov <warnes23@yandex.ru>
2019-06-13 14:26:54 +0300

    index+

--- src/components/components/Index/SectionGift.vue
+++ src/components/components/Index/SectionGift.vue
@@ -19,10 +19,19 @@
         </div>
         
         <div class="gift-check" v-if="giftCheck.length !== 0">
-            <div class="">Вы выбрали подарок:</div>
-            <div v-html="giftCheck"></div>
-            <ui-button label="Получить подарок" color="red" />
+            <div class="header">Вы выбрали подарок:</div>
+            <div v-html="giftCheck" class="name"></div>
+        
+            <router-link :to="{name: 'quiz'}">
+                <ui-button label="Получить подарок" color="red" />
+            </router-link>
+
             <div class="comment text-comment">Ответив на 7 вопросов</div>
+            <div class="comment text-comment">Тест бесплатный. Данные защищены</div>
+
+            <div class="footer-comment text-comment">* Прохождение теста дает возможность получить подарки при оплате заказа</div>
+            <hr>
+            <div class="footer text-comment"><b>Промышленное предприятие "ASTEK"</b> <br> - производим высокоточное промышленное оборудование</div>
         </div>
 
     </div>
@@ -62,8 +71,6 @@ export default {
 .section-gift {
     padding: $m-basic;
 }
-
-
 .header {
     text-align: center;
     margin-top: $m-large;
@@ -80,54 +87,75 @@ export default {
     flex-wrap: wrap;
     justify-content: center;
     margin-top: $m-basic;
-
-    .gift {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        width: 290px;
-        padding: 20px;
-        margin: 20px 10px;
-        box-shadow: $box-shadow-color;
-        cursor: pointer;
-        transition: 0.4s;
-        outline: 3px solid transparent;
-        &:hover {
-            outline: 3px solid transparentize($c-accent, 0.7)
-        }
-        .gift-header {
-            font-family: $ff-header;
-            font-size: $fz-normal;
-            text-transform: uppercase;
-            text-align: center;
-        }
-        .gift-description {
-            margin: $m-basic 0px;
-        }
-
-        img {
-            flex-shrink: 0;
-            width: 100%;
-            height: 200px;
-            display: block;
-            object-fit: cover;
-            object-position: 50% 50%;
-        }
-
-        .component-ui-button {
-            margin-top: $m-basic;
-        }
-    }
-
 }
-
-
+.gift {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 290px;
+    padding: 20px;
+    margin: 20px 10px;
+    box-shadow: $box-shadow-color;
+    cursor: pointer;
+    transition: 0.4s;
+    outline: 3px solid transparent;
+    &:hover {
+        outline: 3px solid transparentize($c-accent, 0.7)
+    }
+    .gift-header {
+        font-family: $ff-header;
+        font-size: $fz-normal;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    .gift-description {
+        margin: $m-basic 0px;
+    }
+    img {
+        flex-shrink: 0;
+        width: 100%;
+        height: 200px;
+        display: block;
+        object-fit: cover;
+        object-position: 50% 50%;
+    }
+    .component-ui-button {
+        margin-top: $m-basic;
+    }
+}
 .gift-check{
+    text-align: center;
+    a {
+        text-decoration: none;
+    }
+    .header {
+        font-size: $fz-large;
+        font-family: $ff-header;
+        text-transform: uppercase;
+    }
+    .name {
+        font-size: $fz-large;
+        font-family: $ff-header;
+        text-transform: uppercase;
+        color: $c-accent;
+    }
     .component-ui-button {
         line-height: 1.7;
-        margin: 10px auto;
+        margin: $m-basic auto 0px auto;
+    }
+    .footer-comment {
+        margin-top: $m-large * 1.5;
+        line-height: 1.7;
+    }
+    .footer {
+        line-height: 1.7;
+    }
+    hr {
+        max-width: 200px;
+        margin: $m-normal auto;
+        height: 1px;
+        border: none;
+        background-color: $c-unaccented;
     }
 }
-
-
 </style>
